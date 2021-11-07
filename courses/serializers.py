7d5e18e36bac3528c403b1afae4f9fdbedd3f232
samul-1 <bsamuele00@gmail.com>
2021-11-08 00:59:10 +0100

    created basic serializers for most entities

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from courses.models import Course, Event, Exercise, ExerciseChoice
+
 
 class HiddenFieldsModelSerializer(serializers.ModelSerializer):
     """
@@ -12,3 +14,43 @@ class HiddenFieldsModelSerializer(serializers.ModelSerializer):
         if context is not None and context.get("show_hidden_fields", False):
             self.Meta.fields.extend(self.Meta.hidden_fields)
         super().__init__(*args, **kwargs)
+
+
+class CourseSerializer(HiddenFieldsModelSerializer):
+    class Meta:
+        model = Course
+        fields = ["name", "description", "creator"]
+        hidden_fields = ["visible", "teachers"]
+
+
+class ExerciseSerializer(HiddenFieldsModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = ["text", "exercise_type", "tags"]
+        hidden_fields = ["solution", "draft"]
+        # type_specific_fields = {
+        #     Exercise.AGGREGATED: "sub_exercises",
+        #     Exercise.MULTIPLE_CHOICE_SINGLE_POSSIBLE: "choices",
+        # }
+
+
+class ExerciseChoiceSerializer(HiddenFieldsModelSerializer):
+    class Meta:
+        model = ExerciseChoice
+        fields = ["text"]
+        hidden_fields = ["correct"]
+
+
+class EventSerializer(HiddenFieldsModelSerializer):
+    class Meta:
+        model = Event
+        fields = [
+            "name",
+            "instructions",
+            "begin_timestamp",
+            "end_timestamp",
+            "event_type",
+            "progression_rule",
+            "state",
+        ]
+        hidden_fields = ["limit_access_to", "template"]
