74c432dffe30f24bc91284cf664b42d86c62b406
samul-1 <bsamuele00@gmail.com>
2021-11-09 16:38:16 +0100

    wrote some admin classes

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import *
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+
+class ExerciseChoiceInline(admin.TabularInline):
+    model = ExerciseChoice
+
+
+class ExerciseTestCaseInline(admin.TabularInline):
+    model = ExerciseTestCase
+
+
+class SubExerciseInline(admin.TabularInline):
+    model = Exercise
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    inlines = [
+        ExerciseChoiceInline,
+        ExerciseTestCaseInline,
+        SubExerciseInline,
+    ]
+
+
+class EventTemplateRuleClauseInline(admin.StackedInline):
+    model = EventTemplateRuleClause
+
+
+class EventTemplateRuleInline(admin.TabularInline):
+    model = EventTemplateRule
+    inlines = [EventTemplateRuleClauseInline]
+
+
+@admin.register(EventTemplate)
+class EventTemplateAdmin(admin.ModelAdmin):
+    inlines = [EventTemplateRuleInline]
