ec3dcfa04cd4fc8f4c52715d710cb23425b72b9a
samul-1 <bsamuele00@gmail.com>
2022-02-16 13:29:06 +0100

    implemented ability to swap models in OrderableModel

--- courses/abstract_models.py
+++ courses/abstract_models.py
@@ -1,14 +1,68 @@
-class OrderableModel(models.Model):
-    ORDER_WITH_RESPECT_TO_FIELD = ""
+from core.models import UUIDModel
+from django.core.exceptions import ValidationError
+from django.db import models, transaction
+
+
+class TrackFieldsMixin(models.Model):
+    """
+    Abstract model used to track changes to a model's fields before
+    writing those changes to the db
+    """
+
+    TRACKED_FIELDS = []  # list of field names
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def from_db(cls, db, field_names, values):
+        instance = super().from_db(db, field_names, values)
+        for fieldname in cls.TRACKED_FIELDS:
+            setattr(instance, f"_old_{fieldname}", getattr(instance, fieldname))
+
+        return instance
+
+
+class OrderableModel(TrackFieldsMixin):
+    ORDER_WITH_RESPECT_TO_FIELD = ""  # field name
+    TRACKED_FIELDS = ["_ordering"]
 
     _ordering = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
 
     def save(self, *args, **kwargs):
         if self.pk is None:  # creating new instance
             # automatically populate the ordering field based on the ordering of siblings
             setattr(self, "_ordering", self.get_ordering_position())
 
-        return super().save(*args, **kwargs)
+        if self._old__ordering != self._ordering:
+            # get model instance that currently has _ordering value
+            # that's being assigned to this instance
+            to_be_swapped = type(self).objects.get(
+                _ordering=self._ordering,
+                **{
+                    self.ORDER_WITH_RESPECT_TO_FIELD: getattr(
+                        self, self.ORDER_WITH_RESPECT_TO_FIELD
+                    )
+                },
+            )
+            self.swap_ordering_with(to_be_swapped)
+
+        else:
+            super().save(*args, **kwargs)
+
+    def swap_ordering_with(self, other):
+        if not isinstance(other, type(self)) or getattr(
+            self, self.ORDER_WITH_RESPECT_TO_FIELD
+        ) != getattr(other, self.ORDER_WITH_RESPECT_TO_FIELD):
+            raise ValidationError("Cannot swap with " + str(other))
+
+        with transaction.atomic():
+            self._ordering, other._ordering = other._ordering, self._ordering
+            other.save()
+            self.save()
 
     def get_ordering_position(self):
         # filter to get parent
@@ -23,9 +77,6 @@ class OrderableModel(models.Model):
 
         max_ordering = siblings.aggregate(max_ordering=Max("_ordering"))["max_ordering"]
         return max_ordering + 1 if max_ordering is not None else 0
-
-    class Meta:
-        abstract = True
 
 
 class TimestampableModel(models.Model):
