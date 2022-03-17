6409fd75598edd2d783dbed5cb7104868689006b
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-03-17 17:55:58 +0100

    temporarily enabled browsable api auth

--- core/settings/base.py
+++ core/settings/base.py
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         # "rest_framework.authentication.TokenAuthentication",
         "oauth2_provider.contrib.rest_framework.OAuth2Authentication",  # django-oauth-toolkit >= 1.0.0
-        # "rest_framework.authentication.SessionAuthentication",  # for browsable api
+        "rest_framework.authentication.SessionAuthentication",  # for browsable api
         "drf_social_oauth2.authentication.SocialAuthentication",
     ),
     "DATETIME_FORMAT": "%Y-%m-%d %H:%M:%S",
