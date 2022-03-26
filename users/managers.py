bff4fea2b3c24bdd5979dbc6d5cabf31614a1ece
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-03-26 12:24:08 +0100

    changed user manager from which custom user manager inherits

--- users/managers.py
+++ users/managers.py
@@ -2,7 +2,9 @@ from django.db import models
 
 from users.querysets import UserQuerySet
 
+from django.contrib.auth.models import UserManager
 
-class UserManager(models.Manager):
+
+class UserManager(UserManager):
     def get_queryset(self):
         return UserQuerySet(self.model, using=self._db)
