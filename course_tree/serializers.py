e4c937fe6dc68270ce769b99a08ce2fe8ca75049
Samuele <bsamuele00@gmail.com>
2022-11-11 16:22:16 +0100

    installed bandit & removed unused imports

--- course_tree/serializers.py
+++ course_tree/serializers.py
@@ -1,4 +1,3 @@
-from django.db.models import Exists, OuterRef
 from rest_framework import serializers
 
 from .models import (
@@ -6,27 +5,24 @@ from .models import (
     TopicNode,
     LessonNode,
     FileNode,
-    BaseCourseTreeNode,
 )
 from courses.serializer_fields import RecursiveField, FileWithPreviewField
 from rest_polymorphic.serializers import PolymorphicSerializer
-import importlib
-import inspect
-import os
 
-from rest_framework.fields import Field
-from rest_framework.serializers import BaseSerializer, SerializerMethodField
 from rest_framework import serializers
 
 
 class CourseTreeNodeSerializer(serializers.ModelSerializer):
+    include_children = True
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields["children"] = serializers.ListSerializer(
-            read_only=True,
-            child=RecursiveField(to="CourseTreeNodePolymorphicSerializer"),
-        )
+        if self.include_children:
+            self.fields["children"] = serializers.ListSerializer(
+                read_only=True,
+                child=RecursiveField(to="CourseTreeNodePolymorphicSerializer"),
+            )
 
         self.fields["parent_id"] = serializers.IntegerField()
 
