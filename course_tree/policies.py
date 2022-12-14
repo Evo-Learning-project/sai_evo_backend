ccb222bb75018fafc1df39a4f742ccdf2fa5b0c6
Samuele <bsamuele00@gmail.com>
2022-12-14 01:47:19 +0100

    fixed permissions for deleting & reordering nodes

--- course_tree/policies.py
+++ course_tree/policies.py
@@ -17,18 +17,34 @@ class TreeNodePolicy(BaseAccessPolicy):
             "condition": "has_create_permission_over_resource_type",
         },
         {
-            "action": ["update", "partial_update", "move", "destroy"],
+            "action": ["update", "partial_update",],
             "principal": ["authenticated"],
             "effect": "allow",
             "condition": "has_update_permission_over_resource_type",
         },
-        # {
-        #     "action": ["update", "partial_update"],
-        #     "principal": ["authenticated"],
-        #     "effect": "allow",
-        #     "condition": "has_teacher_privileges:update_course",
-        # },
+        {
+            "action": ["destroy", "move"],
+            "principal": ["authenticated"],
+            "effect": "allow",
+            "condition": "has_teacher_privileges:manage_course_tree_nodes",
+        },
     ]
+
+    def can_destroy_nodes(self, request, view, action):
+        resource_type = request.data.get("resourcetype")
+
+        if resource_type is None:
+            # let serializer handle invalid input
+            return True
+
+        if resource_type == "RootCourseTreeNode":
+            # users cannot create root nodes directly
+            return False
+
+        # TODO use different permissions depending on the resource type (distinguish between resources that can only be created by teachers)
+        return check_privilege(
+            request.user, view.kwargs.get("course_pk"), MANAGE_COURSE_TREE_NODES
+        )
 
     def has_create_permission_over_resource_type(self, request, view, action):
         resource_type = request.data.get("resourcetype")
