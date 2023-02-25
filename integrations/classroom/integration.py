a52b6978cb318f73fe39211a7dfb9014fe60b86b
Samuele <bsamuele00@gmail.com>
2023-02-25 13:55:27 +0100

    installed deps for classroom integration & added some extra stub code

--- integrations/classroom/integration.py
+++ integrations/classroom/integration.py
@@ -3,10 +3,24 @@ from users.models import User
 from course_tree.models import AnnouncementNode, LessonNode
 from courses.models import Event
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 
 class GoogleClassroomIntegration(BaseEvoIntegration):
+    SCOPES = [
+        "https://www.googleapis.com/auth/classroom.courses.readonly",
+        "https://www.googleapis.com/auth/classroom.announcements",
+        "https://www.googleapis.com/auth/classroom.courseworkmaterials",
+        "https://www.googleapis.com/auth/classroom.coursework.students",
+    ]
+
     def get_service(self):
-        ...
+        creds = self.get_credentials()
+        return build("classroom", "v1", credentials=creds)
 
     def get_credentials(self):
         ...
