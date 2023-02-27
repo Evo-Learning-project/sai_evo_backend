8bd55dd8fbab20ee1034ae0b4c3d122bfc2d8518
Samuele <bsamuele00@gmail.com>
2023-02-27 16:35:17 +0100

    more methods on classroom integration class

--- integrations/classroom/integration.py
+++ integrations/classroom/integration.py
@@ -1,13 +1,18 @@
+from integrations.exceptions import MissingIntegrationParameters
 from integrations.integration import BaseEvoIntegration
 from users.models import User
 from course_tree.models import AnnouncementNode, LessonNode
-from courses.models import Event
+from courses.models import Event, Course
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+import os
+
+import environ
 
 
 class GoogleClassroomIntegration(BaseEvoIntegration):
@@ -18,11 +23,41 @@ class GoogleClassroomIntegration(BaseEvoIntegration):
         "https://www.googleapis.com/auth/classroom.coursework.students",
     ]
 
-    def get_service(self):
-        creds = self.get_credentials()
+    def get_classroom_course_from_evo_course(self, course: Course):
+        ...
+
+    def get_client_config(self):
+        env = environ.Env()
+
+        client_id = os.environ.get("GOOGLE_CLASSROOM_INTEGRATION_CLIENT_ID")
+        project_id = os.environ.get("GOOGLE_CLASSROOM_INTEGRATION_PROJECT_ID")
+        client_secret = os.environ.get("GOOGLE_CLASSROOM_INTEGRATION_CLIENT_SECRET")
+        redirect_uris = env.list(
+            "GOOGLE_CLASSROOM_INTEGRATION_REDIRECT_URIS", default=[]
+        )
+
+        if any(c is None for c in (client_id, project_id, client_secret)):
+            raise MissingIntegrationParameters(
+                "Missing parameters for Google Classroom integration"
+            )
+
+        return {
+            "installed": {
+                "client_id": client_id,
+                "project_id": project_id,
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_secret": client_secret,
+                "redirect_uris": redirect_uris,
+            }
+        }
+
+    def get_service(self, user: User):
+        creds = self.get_credentials(user)
         return build("classroom", "v1", credentials=creds)
 
-    def get_credentials(self):
+    def get_credentials(self, user: User):
         ...
 
     def on_announcement_published(self, user: User, announcement: AnnouncementNode):
@@ -32,4 +67,13 @@ class GoogleClassroomIntegration(BaseEvoIntegration):
         ...
 
     def on_lesson_published(self, user: User, lesson: LessonNode):
+        ...
+
+    def get_courses_taught_by(self, user: User):
+        ...
+
+    def get_course_students(self, course: Course):
+        ...
+
+    def get_course_teachers(self, course: Course):
         ...
