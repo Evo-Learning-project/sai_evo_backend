04cbe8a450ba3578bc5b48ecd13a05514e67d61d
Samuele <bsamuele00@gmail.com>
2023-03-01 15:41:16 +0100

    first draft of integration registry class

--- integrations/integration.py
+++ integrations/integration.py
@@ -3,8 +3,25 @@ from courses.models import Event, EventParticipation
 from users.models import User
 from course_tree.models import AnnouncementNode, LessonNode
 
+import inspect
+
 
 class BaseEvoIntegration(ABC):
+    ACTION_HANDLER_PREFIX = "on_"
+
+    @classmethod
+    def get_available_actions(cls):
+        """
+        Returns a list of actions that the integration is able to handle.
+        The actions can be handled by calling a method named "on_" + name of the action
+        on the handler class
+        """
+        ret = []
+        for name, _ in inspect.getmembers(cls, predicate=inspect.isfunction):
+            if name.startswith(cls.ACTION_HANDLER_PREFIX):
+                ret.append(name[len(cls.ACTION_HANDLER_PREFIX) :])
+        return ret
+
     @abstractmethod
     def get_credentials(self, user: User):
         ...
--- integrations/registry.py
+++ integrations/registry.py
@@ -1,2 +1,49 @@
-def get_integration_types():
-    return ["GOOGLE_CLASSROOM"]
+from typing import Callable, Iterable, Type
+from courses.models import Course
+from integrations.classroom.integration import GoogleClassroomIntegration
+from integrations.integration import BaseEvoIntegration
+
+from integrations.classroom.models import GoogleClassroomCourseTwin
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class IntegrationRegistry:
+    def schedule_integration_method_execution(self, method: Callable, **kwargs):
+        # TODO schedule a celery task
+        method(**kwargs)
+
+    def dispatch(self, action_name: str, course: Course, **kwargs):
+        integrations = self.get_enabled_integrations_for(course)
+
+        for integration_cls in integrations:
+            integration = integration_cls()
+            if action_name in integration.get_available_actions():
+                method = getattr(
+                    integration, integration_cls.ACTION_HANDLER_PREFIX + action_name
+                )
+                self.schedule_integration_method_execution(method, **kwargs)
+            else:
+                logger.warning(
+                    f"{str(integration_cls)} doesn't support action {action_name}"
+                )
+
+    def get_enabled_integrations_for(
+        self,
+        course: Course,
+    ) -> Iterable[Type[BaseEvoIntegration]]:
+        """
+        Returns a list of subclasses of `BaseEvoIntegration` representing integrations
+        enabled for the given course.
+
+        """
+        ret = []
+
+        # NOTE currently, the only integration available is the Google Classroom one, so
+        # this is hard coded. in the future, we may dynamically load available integrations
+        if GoogleClassroomCourseTwin.objects.filter(course=course).exists():
+            ret.append(GoogleClassroomIntegration)
+
+        return ret
